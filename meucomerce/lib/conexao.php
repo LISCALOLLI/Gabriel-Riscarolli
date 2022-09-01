5d82f940287565f546c0572251aa224023507bf2
gabriel <gabriel.riscarolli@unidavi.edu.br>
2022-09-01 12:49:40 -0300

    NorthWind

--- meucomerce/lib/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$username = 'root';
-$password = '';
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=sei lá', $username, $password);
-} catch(PDOException $e) {
-    echo 'ERROR: ' . $e->getMessage();
-}
