3e13f420cbb8c0834498d78031790dda4469d023
gabriel <gabriel.riscarolli@unidavi.edu.br>
2022-09-01 12:53:26 -0300

    NorthWind

---
+++ meucomerce/m/categorias.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+</head>
+<body>
+<h1>Listagem de produtos</h1>
+<table border="1" class="fds"><style> .fds { color: white; background-color: black; border-color: red; text-align: center;margin: 0 auto;} body {background-color: black; } h1 { text-align: center; color: white;}</style>
+    <tr>
+        <td>Código</td>
+        <td>Categoria</td>
+        <td>Categoria Pai</td>
+    </tr>
+<?php
+    $sql = "SELECT c.id, c.categoria_pai, c.descricao from categorias c " ;
+    $consulta = $conn->prepare($sql);
+    $resultado = $consulta->execute();
+    foreach($consulta as $linha) {
+            ?>
+                <tr>
+                    <td><?php echo $linha['id']; ?></td> 
+                    <td><?php echo $linha['descricao']; ?></td>
+                    <td><?php echo $linha['categoria_pai']; ?></td>
+                    <td><?php echo "<a href=\"?pagina=p_atualizacat&id={$linha['id']}\">Atualizar</a>"; ?></td>
+                    <td><?php echo " <a href=\"?pagina=p_deletacat&id={$linha['id']}\">Deletar</a>"; ?></td>
+                </tr>
+<?php
+    }
+    ?>
+
+</table>
+</body>
+</html>
---
+++ meucomerce/menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>menu</title>
+</head>
+<body><style> body{background-color: black;} </style>
+    
+
+<a href="?pagina=p_meulista">Lista de Produtos</a>
+<a href="?pagina=p_meucadastro">Cadastro de Produtos</a>
+<a href="?pagina=p_cadastroc">Cadastro de Categorias Pai</a>
+<a href="?pagina=p_categorias">Lista de Categorias</a>
+<hr>
+</body>
+</html>
