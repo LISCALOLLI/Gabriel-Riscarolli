c3a0083d38af30767ab292e370713574e7e39a38
gabriel <gabriel.riscarolli@unidavi.edu.br>
2022-09-14 12:45:28 -0300

    NorthWind

--- meucomerce/m/lista.php
+++ meucomerce/m/lista.php
@@ -10,13 +10,13 @@
 <h1>Listagem de produtos</h1>
 <table border="1" class="fds"><style> .fds { color: white; background-color: black; border-color: red; text-align: center;margin: 0 auto;} body {background-color: black; } h1 { text-align: center; color: white;}</style>
     <tr>
-        <td>Código</td>
+        <td>Imagem</td>
+        <td>Código</td> 
         <td>Nome</td>
         <td>Características</td>
         <td>Categoria</td>
         <td>Valor</td>        
         <td>Estoque</td>
-        <td>Imagem</td>
         <td>Resumo</td>
         <td>Atualizar</td>
         <td>Deletar</td>
@@ -28,13 +28,13 @@
     foreach($consulta as $linha) {
             ?>
                 <tr>
+                    <td> <img src="<?php echo $linha['imagem']; ?>"></td>
                     <td><?php echo $linha['codigo']; ?></td>
                     <td><?php echo $linha['nome']; ?></td>
                     <td><?php echo $linha['caracteristicas']; ?></td>   
                     <td><?php echo $linha['descricao']; ?></td>
                     <td><?php echo $linha['valor']; ?></td>                    
-                    <td><?php echo $linha['estoque']; ?></td>
-                    <td> <img src="<?php echo $linha['imagem']; ?>"></td>
+                    <td><?php echo $linha['estoque']; ?></td>                    
                     <td><?php echo $linha['resumo']; ?></td>
                     <td><?php echo "<a href=\"?pagina=p_atualiza&id={$linha ['codigo']}\">Atualizar</a>"; ?></td>
                     <td><?php echo " <a href=\"?pagina=p_deleta&id={$linha['codigo']}\">Deletar</a>"; ?></td>
